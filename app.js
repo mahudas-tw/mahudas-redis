3b45b2ca17982a33b0022a59f9ad3422689a75de
DiroKao <dirokao@gmail.com>
2023-02-20 15:25:18 +0800

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+const redis = require('./lib/redis');
+
+module.exports = (app) => {
+    // 當config被載入後就開始進行連線
+    app.on('configDidLoad', () => {
+        app.redis = redis.createConnect(app);
+    });
+
+    // 離開app時切斷連線
+    app.on('beforeClose', async () => {
+        await redis.disconnect();
+    });
+};
---
+++ config/config.default.js
@@ -0,0 +1,28 @@
+exports.redis = {
+    // Single Redis
+    // client: {
+    //   host: 'host',
+    //   port: 'port',
+    //   family: 'user',
+    //   password: 'password',
+    //   db: 'db',
+    // },
+
+    // Cluster Redis
+    // client: {
+    //   cluster: true,
+    //   nodes: [{
+    //     host: 'host',
+    //     port: 'port',
+    //     family: 'user',
+    //     password: 'password',
+    //     db: 'db',
+    //   },{
+    //     host: 'host',
+    //     port: 'port',
+    //     family: 'user',
+    //     password: 'password',
+    //     db: 'db',
+    //   },
+    // ]},
+};
